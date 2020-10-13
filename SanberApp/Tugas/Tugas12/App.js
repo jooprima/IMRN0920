d4151b83d9cd32dcc9bfb9bdeb7ad3aebb18b726
jooprima <jooprima@gmail.com>
2020-10-14 00:51:47 +0700

    create navBar

--- SanberApp/Tugas/Tugas12/App.js
+++ SanberApp/Tugas/Tugas12/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.navBar}></View>
+        <View style={styles.navBar}>
+          <Image
+            source={require("../Image/logo.png")}
+            style={{ width: 98, height: 22 }}
+          />
+          <View style={styles.rightNav}>
+            <TouchableOpacity>
+              <Icon style={styles.navItem} name="search" size={25} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon style={styles.navItem} name="account-circle" size={25} />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -14,10 +28,21 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
   },
   navBar: {
     height: 55,
     backgroundColor: "white",
     elevation: 3,
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  rightNav: {
+    flexDirection: "row",
+  },
+  navItem: {
+    marginLeft: 25,
   },
 });
