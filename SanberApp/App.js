ba1c64d2f95d30b98a5dbc5a96739ffb332a19c1
jooprima <jooprima@gmail.com>
2020-10-14 00:01:41 +0700

    add folder tugas12

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Component from './Tugas/Tugas12/App';
+import YoutubeUI from './Tugas/Tugas12/App';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <YoutubeUI/>
+    // <Component />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
---
+++ SanberApp/Tugas/Tugas12/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
